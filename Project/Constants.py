411991e3a2ca5d3fde78e6021096679b05a3760e
Shubh A Chudasama <shubh.cs@proton.me>
2021-03-12 14:08:29 +0530

    Add files via upload

---
+++ Project/Constants.py
@@ -0,0 +1,9 @@
+# Constants with arbitrary integer values
+MALE_VOICE = 1
+FEMALE_VOICE = 2
+
+# Constants with absolute integer values
+# DO NOT CHANGE THESE!!!
+NORMAL_SPEED = 150
+SLOW_SPEED = 100
+FAST_SPEED = 200
