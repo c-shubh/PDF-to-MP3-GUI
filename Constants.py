d23ca57e3a8179f5e2bae189d6dc54b8ac104c99
Dhruti-C <59955751+Dhruti-C@users.noreply.github.com>
2021-03-12 14:09:06 +0530

    Delete Constants.py

--- Constants.py
+++
@@ -1,9 +0,0 @@
-# Constants with arbitrary integer values
-MALE_VOICE = 1
-FEMALE_VOICE = 2
-
-# Constants with absolute integer values
-# DO NOT CHANGE THESE!!!
-NORMAL_SPEED = 150
-SLOW_SPEED = 100
-FAST_SPEED = 200
